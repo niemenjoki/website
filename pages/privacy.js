80c62e94fc978a4f8a54d77159149e0de8004546
Joonas Jokinen <joonas@joonasjokinen.fi>
2024-01-22 15:13:22 +0200

    Add privacy policy

---
+++ pages/privacy.js
@@ -0,0 +1,81 @@
+import Layout from '@/components/Layout';
+import classes from '@/styles/PrivacyPage.module.css';
+import Link from 'next/link';
+
+const PrivacyPage = () => {
+  return (
+    <Layout
+      title={'Privacy policy | Joonas Niemenjoki'}
+      language="en"
+      i18n={`https://niemenjoki.fi/tietosuoja`}
+    >
+      <div className={classes.PrivacyPage}>
+        <h1>Privacy Policy</h1>
+        <p>
+          <Link href="/tietosuoja">
+            <a>🇫🇮 Lue tämä tietosuojaseloste suomeksi</a>
+          </Link>
+        </p>
+        <p>
+          <em>Last updated: 22nd January 2024</em>
+        </p>
+
+        <p>
+          This a personal website operated by Joonas Niemenjoki. This privacy
+          policy informs you of policies regarding the collection, use, and
+          disclosure of Personal Information on this website.
+        </p>
+        <h2>Information Collection and Use</h2>
+        <p>
+          This website does not not directly collect any personally identifiable
+          information. However, it utilizes third-party services that may
+          collect information. You're free to continue using this website even
+          if you use ad blockers or any other services that prevent the use of
+          data collecting third party services on this website.
+        </p>
+        <h2>Google AdSense and Google Analytics</h2>
+        <p>
+          Google AdSense and Google Analytics are used on this site to serve ads
+          and analyze user behavior. These services may collect data such as IP
+          addresses, browser type, browsing habits, and device information.
+          Please refer to Google's privacy policies for more details on how they
+          handle this data:{' '}
+          <a
+            href="https://support.google.com/adsense/topic/13821022?hl=en"
+            target="_blank"
+          >
+            Google AdSense Privacy & Terms
+          </a>{' '}
+          and{' '}
+          <a
+            href="https://support.google.com/analytics/topic/2919631?hl=en"
+            target="_blank"
+          >
+            Google Analytics Privacy & Terms.
+          </a>
+        </p>
+        <h2>LocalStorage</h2>
+        <p>
+          When you switch between light and dark modes or Finnish and English on
+          this website, we store your preferences in your browser's
+          localStorage. This way, we can automatically use the theme and
+          language of your choice next time you visit out site. This data is
+          stored locally on the your device and is not transfered anywhere.
+        </p>
+        <h2>Cookies</h2>
+        <p>
+          This website itself doesn't use cookies but it utilizes third-party
+          services that use them. You can instruct your browser to refuse all
+          cookies or to indicate when a cookie is being sent.
+        </p>
+        <h2>Contact</h2>
+        <p>
+          If you have any questions about this Privacy Policy, please contact me
+          at joonas.niemenjoki(a)gmail.com.
+        </p>
+      </div>
+    </Layout>
+  );
+};
+
+export default PrivacyPage;
---
+++ pages/tietosuoja.js
@@ -0,0 +1,84 @@
+import Layout from '@/components/Layout';
+import classes from '@/styles/PrivacyPage.module.css';
+import Link from 'next/link';
+
+const PrivacyPage = () => {
+  return (
+    <Layout
+      title={'Tietosuojaseloste | Joonas Niemenjoki'}
+      language="en"
+      i18n={`https://niemenjoki.fi/privacy`}
+    >
+      <div className={classes.PrivacyPage}>
+        <h1>Tietosuojaseloste</h1>
+        <p>
+          <em>Päivitetty: 22. tammikuuta 2024</em>
+        </p>
+        <p>
+          <Link href="/privacy">
+            <a>🇬🇧 Read this privacy policy in English</a>
+          </Link>
+        </p>
+        <p>
+          Tämä on Joonas Niemenjoen ylläpitämä henkilökohtainen verkkosivusto.
+          Tässä tietosuojaselosteessa kerrotaan käytännöistä, jotka liittyvät
+          henkilötietojesi keräämiseen ja käyttöön tällä verkkosivustolla.
+        </p>
+        <h2>Tietojen kerääminen ja käyttö</h2>
+        <p>
+          Tämä verkkosivusto ei suoraan kerää mitään henkilökohtaisesti
+          tunnistettavaa tietoa. Se kuitenkin käyttää kolmannen osapuolen
+          palveluita, jotka saattavat kerätä tietoa. Voit jatkaa tämän
+          verkkosivuston käyttöä vapaasti, vaikka käyttäisit mainosten
+          esto-ohjelmia tai muita palveluita, jotka estävät tietojen keräämistä
+          kolmannen osapuolen palveluilla tällä verkkosivustolla.
+        </p>
+        <h2>Google AdSense ja Google Analytics</h2>
+        <p>
+          Tällä sivustolla käytetään Google AdSensea ja Google Analyticsia
+          mainosten näyttämiseen ja käyttäjien käyttäytymisen analysointiin.
+          Nämä palvelut voivat kerätä tietoja, kuten IP-osoitteet, selaintyyppi,
+          selaustottumukset ja laitetiedot. Lisätietoja siitä, miten nämä
+          palvelut käsittelevät tietojasi, löydät Googlen
+          tietosuojakäytännöistä:{' '}
+          <a
+            href="https://support.google.com/adsense/topic/13821022?hl=fi"
+            target="_blank"
+          >
+            Google AdSensen tietosuojakäytännöt
+          </a>{' '}
+          and{' '}
+          <a
+            href="https://support.google.com/analytics/topic/2919631?hl=en"
+            target="_blank"
+          >
+            Google Analyticsin tietosuojakäytännöt
+          </a>
+        </p>
+        <h2>LocalStorage</h2>
+        <p>
+          Kun vaihdat valo- ja pimeätilojen tai suomen ja englannin kielen
+          välillä tällä verkkosivustolla, tallennamme mieltymyksesi selaimesi
+          paikalliseen tallennustilaan (localStorage). Tällä tavoin voimme
+          automaattisesti käyttää valitsemaasi teemaa ja kieltä seuraavalla
+          sivuston käyntikerralla. Tämä tieto tallennetaan paikallisesti
+          laitteellesi eikä sitä siirretä minnekään.
+        </p>
+        <h2>Evästeet</h2>
+        <p>
+          Tämä sivusto ei suoraan käytä evästeitä, mutta se käyttää kolmannen
+          osapuolen palveluita, jotka käyttävät evästeitä. Voit ohjeistaa
+          selaimesi kieltämään kaikki evästeet tai ilmoittamaan, kun evästeitä
+          lähetetään.
+        </p>
+        <h2>Yhteys</h2>
+        <p>
+          Jos sinulla on kysymyksiä tästä tietosuojakäytännöstä, ota yhteyttä
+          minuun sähköpostitse osoitteeseen joonas.niemenjoki(a)gmail.com.
+        </p>
+      </div>
+    </Layout>
+  );
+};
+
+export default PrivacyPage;
