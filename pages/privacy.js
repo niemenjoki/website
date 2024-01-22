dbd3f15503cac61926644796c7b96d8acbf68b68
Joonas Jokinen <joonas@joonasjokinen.fi>
2024-01-22 16:11:43 +0200

    Add entity escaping

--- pages/privacy.js
+++ pages/privacy.js
@@ -29,17 +29,17 @@ const PrivacyPage = () => {
         <p>
           This website does not not directly collect any personally identifiable
           information. However, it utilizes third-party services that may
-          collect information. You're free to continue using this website even
-          if you use ad blockers or any other services that prevent the use of
-          data collecting third party services on this website.
+          collect information. You&apos;re free to continue using this website
+          even if you use ad blockers or any other services that prevent the use
+          of data collecting third party services on this website.
         </p>
         <h2>Google AdSense and Google Analytics</h2>
         <p>
           Google AdSense and Google Analytics are used on this site to serve ads
           and analyze user behavior. These services may collect data such as IP
           addresses, browser type, browsing habits, and device information.
-          Please refer to Google's privacy policies for more details on how they
-          handle this data:{' '}
+          Please refer to Google&apos;s privacy policies for more details on how
+          they handle this data:{' '}
           <a
             href="https://support.google.com/adsense/topic/13821022?hl=en"
             target="_blank"
@@ -57,16 +57,16 @@ const PrivacyPage = () => {
         <h2>LocalStorage</h2>
         <p>
           When you switch between light and dark modes or Finnish and English on
-          this website, we store your preferences in your browser's
+          this website, we store your preferences in your browser&apos;s
           localStorage. This way, we can automatically use the theme and
           language of your choice next time you visit out site. This data is
           stored locally on the your device and is not transfered anywhere.
         </p>
         <h2>Cookies</h2>
         <p>
-          This website itself doesn't use cookies but it utilizes third-party
-          services that use them. You can instruct your browser to refuse all
-          cookies or to indicate when a cookie is being sent.
+          This website itself doesn&apos;t use cookies but it utilizes
+          third-party services that use them. You can instruct your browser to
+          refuse all cookies or to indicate when a cookie is being sent.
         </p>
         <h2>Contact</h2>
         <p>
