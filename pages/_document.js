74f3e09f61ed58748b3ad7001ff9145532aa8cf0
Joonas Jokinen <joonas@joonasjokinen.fi>
2024-02-21 16:39:08 +0200

    Fix non-boolean attribute

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,19 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,700;0,800;1,400;1,700;1,800&display=swap"
           rel="stylesheet"
         />
-      <meta name="google-site-verification" content="QnhqvG850vkdtc4C7pk0jp9JUDowtwf-vVks_iHQLWY" />
+        <meta
+          name="google-site-verification"
+          content="QnhqvG850vkdtc4C7pk0jp9JUDowtwf-vVks_iHQLWY"
+        />
       </Head>
       <body>
         <Main />
