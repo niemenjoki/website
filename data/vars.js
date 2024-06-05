fdf62610a514423c30674fbcd1d279caf1f90584
Joonas Jokinen <joonas@joonasjokinen.fi>
2024-06-05 19:17:00 +0300

    Update compress-cra link after username change

--- data/vars.js
+++ data/vars.js
@@ -1,3 +1,3 @@
 export const POSTS_PER_PAGE = 8;
 export const COMPRESS_CRA_README =
-  'https://raw.githubusercontent.com/jnsjknn/compress-create-react-app/master/README.md';
+  'https://raw.githubusercontent.com/niemenjoki/compress-create-react-app/master/README.md';
