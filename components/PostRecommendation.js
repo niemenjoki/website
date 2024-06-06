30db363a0b93f395031a617d8e4fb3bedb78e500
Joonas Jokinen <joonas@joonasjokinen.fi>
2024-06-06 04:46:16 +0300

    Change recommendations' header text

--- components/PostRecommendation.js
+++ components/PostRecommendation.js
@@ -5,11 +5,7 @@ const PostRecommendation = ({ language, posts = [] }) => {
   if (!posts || posts.length === 0) return <></>;
   return (
     <div className={classes.PostRecommendation}>
-      <h2>
-        {language === 'fi'
-          ? 'Muita aiheeseen liittyviä julkaisuja'
-          : 'Explore Other Related Posts'}
-      </h2>
+      <h2>{language === 'fi' ? 'Muita julkaisuja' : 'Explore Other Posts'}</h2>
       {posts.map((post) => (
         <Post key={post.slug} post={post} compact={false} language={language} />
       ))}
