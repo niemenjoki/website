eb7aac0df0b481686efeeb35bdbcc4cdfcd90828
Joonas Jokinen <joonas@joonasjokinen.fi>
2025-09-25 12:15:43 +0300

    Update next config file to modern format

--- next.config.js
+++ next.config.js
@@ -1,20 +1,6 @@
-const withPlugins = require('next-compose-plugins');
-const withPWA = require('next-pwa');
-
 const nextConfig = {
-  productionBrowserSourceMaps: true,
   reactStrictMode: true,
-  swcMinify: true,
+  productionBrowserSourceMaps: true,
 };
 
-const pwaConfig = {
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-};
-
-module.exports = withPlugins(
-  [
-    [withPWA, { pwa: pwaConfig }],
-  ],
-  nextConfig
-);
+module.exports = nextConfig;
